1d69c8cf1fb77fcea77110d43516f45600aaa24f
burngeek8 <burngeek8@gmail.com>
2014-12-03 20:23:14 -0500

    Algunos cambios
    etiqueta label, el procesar recibe los datos enviados por get

---
+++ procesar.php
@@ -0,0 +1,4 @@
+<?php 
+	//Con este tipo de variable recibo datos mediante la URL si te fijas arriba se concatena los datos y se lo pasa a esta pagina.
+	echo "Usuario: ".$_GET['user']."</br> PassWord: ".$_GET['password'];
+ ?>
