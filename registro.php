a83a183791b2a2f130cd93e9987434a035ab6637
creativepmg <creativepmg5@gmail.com>
2014-12-09 02:03:14 -0500

    yee funciono :D

--- registro.php
+++ registro.php
@@ -1,9 +1,8 @@
 <?php
-	include('conexion.php');
-	$con=mysql_connect($host,$user,$pw)or die("problemas al conectar");
-		mysql_select_db($db,$con)or die("problemas al conectar la bd");
+	$con=mysql_connect('localhost','creative_admin','creativepmg')or die("problemas al conectar");
+		mysql_select_db('creative_desarrollo',$con)or die("problemas al conectar la bd".mysql_error());
 
-		mysql_query("INSERT INTO nombre de la tabla (COLUMNAS A LAS Q SE VA A INSERTAR) VALUES (valores a insertar ejemplo '$_POST[email]','0')",$con);
+		mysql_query("INSERT INTO users (USER,PASS,EMAIL) VALUES ('$_POST[username]','$_POST[password]','$_POST[email]')",$con);
 
 		echo "Los datos fueron registrados correctamente <br />Username: ".$_POST['username']."<br>Password: ".$_POST['password']."</br>Email: ".$_POST['email'];
 ?>
