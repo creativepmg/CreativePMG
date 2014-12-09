9c24c2de57923f9049c77284d598e79df67ba181
burngeek8 <burngeek8@gmail.com>
2014-12-09 02:07:55 -0500

    condicionales usuarios

--- procesar.php
+++ procesar.php
@@ -1,4 +1,20 @@
 <?php 
+
+	if($_POST['user'] = 'administrador')
+	{
+		if($_POST['password'] = '123456')
+		{
+			echo "Bienvenido".$_POST['user'];
+		}
+		else
+		{
+			echo "tu contraseña es erronea"
+		}
+	}
+	else
+	{
+		echo "el usuario esta equivocado";
+	}
 	//Con este tipo de variable recibo datos mediante la URL si te fijas arriba se concatena los datos y se lo pasa a esta pagina.
 	echo "Usuario: ".$_POST['user']."</br> PassWord: ".$_POST['password'];
  ?>
