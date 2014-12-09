e55799aa270e2e8d4adee7cb7f9620397915a35c
burngeek8 <burngeek8@gmail.com>
2014-12-09 01:36:29 -0500

    hacer un insert a la tabla de una base de datos

--- registro.php
+++ registro.php
@@ -1,3 +1,9 @@
 <?php
-	echo "Username: ".$_POST['username']."<br>Password: ".$_POST['password']."</br>Email: ".$_POST['email'];
+	include('conexion.php');
+	$con=mysql_connect($host,$user,$pw)or die("problemas al conectar");
+		mysql_select_db($db,$con)or die("problemas al conectar la bd");
+
+		mysql_query("INSERT INTO nombre de la tabla (COLUMNAS A LAS Q SE VA A INSERTAR) VALUES (valores a insertar ejemplo '$_POST[email]','0')",$con);
+
+		echo "Los datos fueron registrados correctamente <br />Username: ".$_POST['username']."<br>Password: ".$_POST['password']."</br>Email: ".$_POST['email'];
 ?>
