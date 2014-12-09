58be33d45fa76ae58506f36f9057f8fcb2b52810
burngeek8 <burngeek8@gmail.com>
2014-12-09 03:55:45 -0500

    lista de usuarios

--- usuarios.php
+++ usuarios.php
@@ -4,7 +4,13 @@
 		mysql_select_db('desarrollo',$con)or die("problemas al conectar la bd".mysql_error());
 
 	$usuarios=mysql_query("SELECT * FROM users",$con);
-		$arrUsuarios=mysql_fetch_array($usuarios);
-		var_dump($arrUsuarios[]);
-		 	
+		
+		while ($arrUsuarios=mysql_fetch_array($usuarios))
+		{
+			echo "	ID: ".$arrUsuarios['ID']."<br />
+					USER: ".$arrUsuarios['USER']."<br />
+					PASS: ".$arrUsuarios['PASSWORD']."<br />
+					EMAIL: ".$arrUsuarios['EMAIL']."<br /><br />
+					";
+		};	 	
  ?>
