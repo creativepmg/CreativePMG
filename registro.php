0bba8802a46042af757cb333fdc3e072fd30e844
creativepmg <creativepmg5@gmail.com>
2014-12-27 03:07:32 -0800

    webeo

--- registro.php
+++ registro.php
@@ -1,5 +1,5 @@
 <?php 
-	include('conexion.php')
+	include('conexion.php');
 	$con=mysql_connect($host,$user,$pw)or die("problemas al conectar");
 		mysql_select_db($db,$con)or die("problemas al conectar la bd".mysql_error());
 
