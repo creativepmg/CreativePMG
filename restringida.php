d3bfa1eb22083b4405cff0af8d02f45dc56fc858
burngeek8 <burngeek8@gmail.com>
2014-12-27 03:22:06 -0500

    link para cerrar sesion

--- restringida.php
+++ restringida.php
@@ -3,8 +3,10 @@
 <html>
 <head>
 	<title>Administrador</title>
+	<meta charset="UTF-8" />
 </head>
 <body>
 	<h1>Bienvenido <?= $_SESSION['username'] ?></h1>
+	<a href="salir.php">Cerrar Sesión</a>
 </body>
 </html>
