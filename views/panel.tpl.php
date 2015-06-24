4b92dcd79fdae9e747ef10c55df853879093fd15
burngeek8 <burngeek8@gmail.com>
2015-06-23 21:16:05 -0500

    contador de clientes

--- views/panel.tpl.php
+++ views/panel.tpl.php
@@ -1,19 +1,17 @@
-<?php require 'template/inicio.php'; ?>
+<?php 
+	require 'template/inicio.php'; 
+	 $lis_clientes = mysql_query("SELECT * FROM clientes ") 
+             or die("Error en la consulta.." . mysqli_error($con));
+	$countClientes = mysql_num_rows($lis_clientes);
+?>
 	
 	<p class="mensajeParaElUsuario">Bienvenido <strong><?= $arrayUsuario['USER']; ?></strong>, estamos trabajando para habilitarte las aplicaciones que tenemos para ti, se paciente y vuelve luego. </p>
 
-	<div class="estadisticas">		
-		<a class="item">		
-			<div class="titulo"></div>
-			<div class="cantidad"></div>
-		</a>
-	</div>
 	
-	<div class="contenedorIcono">
-		
+	<div class="contenedorIcono">		
 		<div class="iconoClientes"></div>
 		<div class="inf">
-			<div class="numeroClientes"> 100 </div>
+			<div class="numeroClientes"> <?= $countClientes ?> </div>
 			<p>Clientes</p>
 		</div>
 	</div>
