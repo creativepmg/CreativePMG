3cd31d11e6842a3a2b41572a8c53d2783e989655
tfbm89 <elrockerom5@hotmail.com>
2015-07-10 18:29:49 -0500

    arreglo a mantenimiento menus
    arreglo a mantenimiento menus

--- views/mantenimiento-menus.tpl.php
+++ views/mantenimiento-menus.tpl.php
@@ -1,39 +1,36 @@
 <?php require 'template/inicio.php'; ?>
 	
-	<div class="tabla">
-		<div class="titulos">
-			<div class="id">ID</div>
-			<div class="descripcion">DESCRIPCION</div>
-			<div class="enlace">ENLACE</div>
-			<div class="opciones">OPCIONES</div>
-		</div>
-	<?php  while ($arrMenus=mysql_fetch_array($lis_menus)) { ?>
-		<div class="item">
-			<div class="id"><?= $arrMenus['ID_MENU'] ?></div>
-			<div class="descripcion"><?= $arrMenus['DESCRIPCION'] ?></div>
-			<div class="enlace"><?= $arrMenus['URL'] ?></div>
-			<div class="opciones">
-			</div>
-		</div>
-	<?php } ?>
-	</div>
+
+
+<div class="secciones">
+    <div class="datos">
+         <div>
+          <div>ID</div>
+          <p>1</p>
+         </div>
+         <div>
+          <div>DESCRIPCION:</div>
+          <p>TIPO DE SERVICIOS</p>
+        </div>
+         <div>
+          <div>ENLACE:</div>
+          <p>www.google.com</p>
+        </div>
+        <div>
+          <div>OPCIONES</div>
+          <p>dos</p>
+        </div>
+    </div>
+    <div class="opcion1">
+      <div class="borrar">
+      </div>
+    </div>
+    <div class="opcion2">
+       <div class="agregar">
+      </div>
+    </div>
+   </div>	
 	<!-- Cajas de dialogo -->
-	<div id="dNewMenu" class="cajaDialogo">
-		<div class="newMenu formulario">
-			<div class="encabezado">
-				NUEVO MENU	
-				<div class="cerrar"></div>
-			</div>
-			<div class="detalles">
-				<form id="nuevoMenu" method="post">
-					<label>Nombre Menu</label>
-					<input name="nombre_menu" class="vaciar" type="text">
-					<label>Link menu</label>
-					<input name="link" class="vaciar" type="text">
-					<input id="botonNuevoMenu" type="submit">
-				</form>
-			</div>
-		</div>
-	</div>
+	<?php require 'src/form/frm_nuevo_menu.php' ?>
 	<div class="botonNuevo" onclick="mostraCajaDialogo('#dNewMenu')"></div>
 <?php require 'template/fin.php'; ?>
