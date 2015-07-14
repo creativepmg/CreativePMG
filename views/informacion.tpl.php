2ae88e36564c99e07b2ae55fe43439f2fcb55030
tfbm89 <elrockerom5@hotmail.com>
2015-07-14 14:55:04 -0500

    diseño informacion
    diseño informacion

---
+++ views/informacion.tpl.php
@@ -0,0 +1,24 @@
+<?php require 'template/inicio.php'; ?>
+
+
+	<div class="contenedor_informacion">
+
+		<div class="bloques">
+			<h3>NOMBRE BASE DE DATOS</h3>
+			<p>
+				latinopm_desarrollo
+			</p>
+		</div>
+
+		<div class="bloques">
+			<h3>NOMBRE DEL SERVIDOR</h3>
+			<p>
+				dev.serviciolatinopmg.com
+			</p>
+		</div>
+			
+	</div>
+	
+	
+
+<?php require 'template/fin.php'; ?>
