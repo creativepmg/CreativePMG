f83966c760b91e0761e389fee4c21c3dde5bc57c
burngeek8 <burngeek8@gmail.com>
2015-08-03 14:02:26 -0500

    conexiones

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$host 			= "localhost";
-	$user 			= "latinopm_admin";
-	$pw 			= "phillip1707";
-	$db 			= "latinopm_desarrollo";
-	$servidor 		= "dev.serviciolatinopmg.com/";
-?>
--- config/conexion.php
+++ config/conexion.php
@@ -1,7 +1,20 @@
 <?php
-	$host 			= "127.0.0.1";
+	//local
+	/*$host 			= "127.0.0.1";
 	$user 			= "root";
 	$pw 			= "w8uiq9da";
 	$db 			= "serviciolatino_desarrollo";
-	$servidor 		= "dev.serviciolatinopmg.com/";
+	$servidor 		= "dev.serviciolatinopmg.com/";*/
+	//produccion
+	$host 			= "localhost";
+	$user 			= "latinopm_admin";
+	$pw 			= "phillip1707";
+	$db 			= "latinopm_main";
+	$servidor 		= "serviciolatinopmg.com/";
+	//desarrollo
+	/*$host 			= "localhost";
+	$user 			= "latinopm_admin";
+	$pw 			= "phillip1707";
+	$db 			= "latinopm_desarrollo";
+	$servidor 		= "dev.serviciolatinopmg.com/";*/
 ?>
