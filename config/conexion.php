40606bb5b5eabf7cd74445b084fdb5ac6a54fc3f
burngeek8 <burngeek8@gmail.com>
2015-09-03 16:23:08 -0500

    conexion

--- config/conexion.php
+++ config/conexion.php
@@ -2,53 +2,38 @@
 	
 	/* 
 	** Servidor VPS
+	
 	$host 			= "locahost";
 	$user 			= "root";
 	$pw 			= "w8uiq9da";
 	$db 			= "serviciolatino";
 	$servidor 		= "45.55.83.188/serviciolatino";
-<<<<<<< HEAD
 	*/
 
 	/* 
 	** Local equipo Jose Luis
-	*/
-		$host 			= "127.0.0.1";
-		$user 			= "root";
-		$pw 			= "w8uiq9da";
-		$db 			= "serviciolatino_desarrollo";
-		$servidor 		= "127.0.0.1/localhost";
-
-	/*
-	** Produccion
-	$host 			= "localhost";
-=======
-	/* Local equipo Jose Luis
 	$host 			= "127.0.0.1";
 	$user 			= "root";
-<<<<<<< HEAD
-	$pw 			= "";
-	$db 			= "serviciolatino";
-	$servidor 		= "dev.serviciolatinopmg.com/";
-=======
 	$pw 			= "w8uiq9da";
 	$db 			= "serviciolatino_desarrollo";
 	$servidor 		= "127.0.0.1/localhost";
 	*/
->>>>>>> origin/desarrollo
-	//produccion
-	/*$host 			= "localhost";
->>>>>>> origin/desarrollo
+
+	/*
+	** Produccion
+	$host 			= "localhost";
 	$user 			= "latinopm_admin";
 	$pw 			= "phillip1707";
 	$db 			= "latinopm_main";
 	$servidor 		= "serviciolatinopmg.com/";
 	*/
 	
-	//desarrollo
-	/*$host 			= "localhost";
+	/*
+	** Desarrollo
+	*/
+	$host 			= "localhost";
 	$user 			= "latinopm_admin";
 	$pw 			= "phillip1707";
 	$db 			= "latinopm_desarrollo";
-	$servidor 		= "dev.serviciolatinopmg.com/";*/
+	$servidor 		= "dev.serviciolatinopmg.com/";
 ?>
