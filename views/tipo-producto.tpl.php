8d72cb9397c7f6328eea9a29d28779cd8b5c3618
burngeek8 <burngeek8@gmail.com>
2015-09-13 12:47:59 -0500

    tipo de producto listado desde db

--- views/tipo-producto.tpl.php
+++ views/tipo-producto.tpl.php
@@ -1,206 +1,33 @@
 <?php require 'template/inicio.php'; ?>
 	<!-- Contenido mostrado -->
-     <div class="contenedorProductos">
-      <div class="contenedorProducto">
-        <div class="row2">
-          <div class="ContImgProducto">
-            <div class="imgProducto ">
-                <img  src="data:image/png;base64,<?= $user_avatar ?>">    
-                <form action="src/inserts/ins_subir_foto.php" method="post" enctype="multipart/form-data">
-                  <label for="avatar">Subir imagen</label>
-                  <input type="hidden" name="id_usuario" value="<?= $userId ?>">
-                  <input type="file" id="avatar" name="avatar"> 
-                  <input id="btn_subir_avatar" type="submit" value="" style="display: none;">
-                </form>
+    <?php while ($arrProductos=mysqli_fetch_array($lis_productos)) {?>
+        <div class="contenedorProductos">
+            <div class="contenedorProducto">
+                <div class="row2">
+                    <div class="ContImgProducto">
+                        <img src="data:image/png;base64,<?= $arrProductos['IMAGEN'] ?>">
+                        <div class="datosImg">
+                            <div class="decripcion"><?= $arrProductos['DESCRIPCION'] ?></div>
+                            <div class="editar"></div>
+                            <div class="eliminar"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row1">
+                    <div class="cabecera">
+                        <div class="idCabecera">id</div>
+                        <div class="precioCabecera">precio</div>
+                    </div>      
+                    <div class="cuerpo">
+                        <div class="id"><?= $arrProductos['ID_PRODUCTO'] ?></div>
+                        <div class="precio"><?= $arrProductos['PRECIO_VENTA'] ?></div>
+                    </div>
+                </div>
             </div>
-              <div class="datosImg">
-                  <div class="decripcion">
-                      DRONE SYMA X5C-1 EXPLORERS
-                  </div>
-                  <div class="editar">
-                      
-                  </div>
-                  <div class="eliminar">
-                      
-                  </div>
-              </div>
         </div>
-    </div>
-    <div class="row1">
-      <div class="cabecera">
-        <div class="idCabecera">id</div>
-        <div class="precioCabecera">precio</div>
-      </div>
-      
-      <div class="cuerpo">
-          <div class="id">1</div>
-          <div class="precio">20.000</div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="contenedorProductos">
-      <div class="contenedorProducto">
-        <div class="row2">
-          <div class="ContImgProducto">
-            <img src="img/celular.jpg">
-              <div class="datosImg">
-                  <div class="decripcion">
-                      DRONE SYMA X5C-1 EXPLORERS
-                  </div>
-                  <div class="editar">
-                      
-                  </div>
-                  <div class="eliminar">
-                      
-                  </div>
-              </div>
-        </div>
-    </div>
-    <div class="row1">
-      <div class="cabecera">
-        <div class="idCabecera">id</div>
-        <div class="precioCabecera">precio</div>
-      </div>
-      
-      <div class="cuerpo">
-          <div class="id">1</div>
-          <div class="precio">20.000</div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="contenedorProductos">
-      <div class="contenedorProducto">
-        <div class="row2">
-          <div class="ContImgProducto">
-            <img src="img/celular.jpg">
-              <div class="datosImg">
-                  <div class="decripcion">
-                      DRONE SYMA X5C-1 EXPLORERS
-                  </div>
-                  <div class="editar">
-                      
-                  </div>
-                  <div class="eliminar">
-                      
-                  </div>
-              </div>
-        </div>
-    </div>
-    <div class="row1">
-      <div class="cabecera">
-        <div class="idCabecera">id</div>
-        <div class="precioCabecera">precio</div>
-      </div>
-      
-      <div class="cuerpo">
-          <div class="id">1</div>
-          <div class="precio">20.000</div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="contenedorProductos">
-      <div class="contenedorProducto">
-        <div class="row2">
-          <div class="ContImgProducto">
-            <img src="img/celular.jpg">
-              <div class="datosImg">
-                  <div class="decripcion">
-                      DRONE SYMA X5C-1 EXPLORERS
-                  </div>
-                  <div class="editar">
-                      
-                  </div>
-                  <div class="eliminar">
-                      
-                  </div>
-              </div>
-        </div>
-    </div>
-    <div class="row1">
-      <div class="cabecera">
-        <div class="idCabecera">id</div>
-        <div class="precioCabecera">precio</div>
-      </div>
-      
-      <div class="cuerpo">
-          <div class="id">1</div>
-          <div class="precio">20.000</div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="contenedorProductos">
-      <div class="contenedorProducto">
-        <div class="row2">
-          <div class="ContImgProducto">
-            <img src="img/celular.jpg">
-              <div class="datosImg">
-                  <div class="decripcion">
-                      DRONE SYMA X5C-1 EXPLORERS
-                  </div>
-                  <div class="editar">
-                      
-                  </div>
-                  <div class="eliminar">
-                      
-                  </div>
-              </div>
-        </div>
-    </div>
-    <div class="row1">
-      <div class="cabecera">
-        <div class="idCabecera">id</div>
-        <div class="precioCabecera">precio</div>
-      </div>
-      
-      <div class="cuerpo">
-          <div class="id">1</div>
-          <div class="precio">20.000</div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="contenedorProductos">
-      <div class="contenedorProducto">
-        <div class="row2">
-          <div class="ContImgProducto">
-            <img src="img/celular.jpg">
-              <div class="datosImg">
-                  <div class="decripcion">
-                      DRONE SYMA X5C-1 EXPLORERS
-                  </div>
-                  <div class="editar">
-                      
-                  </div>
-                  <div class="eliminar">
-                      
-                  </div>
-              </div>
-        </div>
-    </div>
-    <div class="row1">
-      <div class="cabecera">
-        <div class="idCabecera">id</div>
-        <div class="precioCabecera">precio</div>
-      </div>
-      
-      <div class="cuerpo">
-          <div class="id">1</div>
-          <div class="precio">20.000</div>
-      </div>
-    </div>
-  </div>
-</div>
-  <!-- Nuevo Cliente  -->
-  <?php require 'src/form/frm_nuevo_producto.php' ?>  
+    <?php } ?>
+    <!-- Nuevo Cliente  -->
+    <?php require 'src/form/frm_nuevo_producto.php' ?>  
 
 	<div class="botonNuevo" onclick="mostraCajaDialogo('#dNewProducto')"></div>
 <?php require 'template/fin.php'; ?>
