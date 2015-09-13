f4664c36db6ff4cbdb69057b6bdfb4c0418306a2
tfbm89 <elrockerom5@hotmail.com>
2015-09-13 12:40:22 -0500

    se agrega boton para subir imagenes
    se agrega un boton para subir imagenes

--- views/tipo-producto.tpl.php
+++ views/tipo-producto.tpl.php
@@ -4,7 +4,15 @@
       <div class="contenedorProducto">
         <div class="row2">
           <div class="ContImgProducto">
-            <img src="img/celular.jpg">
+            <div class="imgProducto ">
+                <img  src="data:image/png;base64,<?= $user_avatar ?>">    
+                <form action="src/inserts/ins_subir_foto.php" method="post" enctype="multipart/form-data">
+                  <label for="avatar">Subir imagen</label>
+                  <input type="hidden" name="id_usuario" value="<?= $userId ?>">
+                  <input type="file" id="avatar" name="avatar"> 
+                  <input id="btn_subir_avatar" type="submit" value="" style="display: none;">
+                </form>
+            </div>
               <div class="datosImg">
                   <div class="decripcion">
                       DRONE SYMA X5C-1 EXPLORERS
